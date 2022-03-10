60e28f6a82e965e31fb0657fea59bc9fc41348e5
Antonio Orozco <Antonio Orozco@DESKTOP-262CT4M>
2022-03-09 22:49:14 -0600

    seguimiento de Vproducto

--- src/Datos/vproducto.java
+++ src/Datos/vproducto.java
@@ -1,11 +1,49 @@
-
 package Datos;
 
+import Domain.Productos;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+ 
 public class vproducto {
-    Conexion instanciaMysql = Conexion.getInstance();
-    private static final String SQL_SELECT ="";
-    private static final String SQL_INSERT ="";
-    private static final String SQL_UPDATE ="";
-    private static final String SQL_DELETE ="";
-    
+
+    conexion instanciaMysql = conexion.getInstance();
+    private static final String SQL_SELECT = "select * from productos";
+    private static final String SQL_INSERT = "insert into productos (Nombre,Precio,Existencias) values (?,?,?)";
+    private static final String SQL_UPDATE = "update productos set Nombre = ?,Precio=?,Existencias =? where Id_Producto = ?";
+    private static final String SQL_DELETE = "delete from productos where Id_Producto =?";
+
+    public List<Productos> listar() {
+        Connection conexion = null;
+        PreparedStatement consultaPreparada = null;
+        ResultSet resultado = null;
+        List<Productos> producto = new ArrayList<>();
+        Productos productos;
+
+        try {
+            conexion = instanciaMysql.conectar();
+            consultaPreparada = conexion.prepareStatement(SQL_SELECT);
+            resultado = consultaPreparada.executeQuery();
+
+            while (resultado.next()) {
+                int Id_Producto = resultado.getInt("Id_Producto");
+                String Nombre = resultado.getString("Nombre");
+                double Precio = resultado.getDouble("Precio");
+                int Existencias = resultado.getInt("Existencias");
+
+                productos = new Productos(Id_Producto, Nombre, Precio, Existencias);
+                producto.add(productos);
+
+            }
+        } catch (SQLException error) {
+            System.out.println(error);
+        }
+        return producto;
+
+    }
+
 }
