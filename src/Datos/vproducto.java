f3128f3205fd0ae3d73ffb013733fc001825474d
Antonio Orozco <Antonio Orozco@DESKTOP-262CT4M>
2022-03-09 22:50:07 -0600

    seguimiento de Vproducto

--- src/Datos/vproducto.java
+++ src/Datos/vproducto.java
@@ -8,7 +8,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
- 
+import java.sql.*;
+
 public class vproducto {
 
     conexion instanciaMysql = conexion.getInstance();
